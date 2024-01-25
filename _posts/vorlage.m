7cafa528f25a61a0cda6b939a84ccbb00f692e80
Helen Haveloh <120733305+helsoc7@users.noreply.github.com>
2024-01-25 15:12:34 +0100

    Create vorlage.m

---
+++ _posts/vorlage.m
@@ -0,0 +1,48 @@
+---
+title: "Ihr Titel"
+date: 2024-01-25
+---
+
+
+## Tagesrückblick
+
+### Lernziele für heute:
+
+- [Hier die Lernziele für den Tag auflisten]
+
+### Gelernte Themen/Fächer:
+
+- [Notiere hier, welche Themen oder Fächer du heute bearbeitet hast.]
+
+### Aufgaben erledigt:
+
+- [Liste alle Aufgaben auf, die du heute abgeschlossen hast.]
+
+### Schwierigkeiten:
+
+- [Falls es Herausforderungen oder Schwierigkeiten gab, notiere diese und wie du versucht hast, sie zu bewältigen.]
+
+### Erkenntnisse:
+
+- [Teile deine Erkenntnisse und Lernfortschritte mit.]
+
+## Reflexion
+
+### Was lief gut heute?
+
+- [Beschreibe, was heute besonders gut gelaufen ist.]
+
+### Was kann verbessert werden?
+
+- [Überlege, wie du deinen Lernprozess verbessern kannst.]
+
+### Neue Ziele:
+
+- [Setze dir Ziele für den nächsten Lerntag oder die kommende Woche.]
+
+## Trainer/TA Feedback (falls vorhanden):
+
+- **Feedback vom Trainer/TA:** [Falls ein Trainer/TA dir Feedback gegeben hat, notiere das hier.]
+
+---
+
